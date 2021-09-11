05e05e37407d9703ac4712aa96889f5798a9b475
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-09-11 20:06:45 +0800

    Database updated

--- tool/hiredev.sql
+++ tool/hiredev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2021 at 08:27 PM
+-- Generation Time: Sep 11, 2021 at 02:06 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.20
 
@@ -39,7 +39,7 @@ CREATE TABLE `devapplications` (
 --
 
 INSERT INTO `devapplications` (`appID`, `name`, `contact`, `email`) VALUES
-(8, 'test user', '+601121721029', 'ea@gmail.com');
+(9, 'Arif', '+8801551624231', 'af@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -62,9 +62,9 @@ CREATE TABLE `devs` (
 --
 
 INSERT INTO `devs` (`id`, `name`, `email`, `contact`, `description`, `specialty`, `join_date`) VALUES
-(9, 'Ariful Hasan Khan', 'akt@gmail.com', '+11121721029', 'More than 5 years of experience in front-end and back-end frameworks.', 'Full Stack Web Developer', '2021-09-04 22:53:32'),
-(10, 'Hasanath Jamy', 'hj@gmail.com', '+601121721029', 'Experienced with java, android studio and react native.', 'Android App Developer', '2021-09-04 22:55:08'),
-(11, 'Nagib MD Sarfaraz', 'ng@gmail.com', '14531513515', 'Experienced with ERP systems like Oracle JDE, ServiceNow.', 'System Developer', '2021-09-04 22:56:36');
+(9, 'Ariful Hasan Khan', 'akt@gmail.com', '+8801716437323', 'More than 5 years of experience in front-end and back-end frameworks.', 'Full Stack Web Developer', '2021-09-04 22:53:32'),
+(10, 'Hasanath Jamy', 'hj@gmail.com', '+8801951564221', 'Experienced with java, android studio and react native.', 'Android App Developer', '2021-09-04 22:55:08'),
+(11, 'Nagib MD Sarfaraz', 'ng@gmail.com', '+8801551024231', 'Experienced with ERP systems like Oracle JDE, ServiceNow.', 'System Developer', '2021-09-04 22:56:36');
 
 -- --------------------------------------------------------
 
@@ -84,7 +84,7 @@ CREATE TABLE `hirerecord` (
 --
 
 INSERT INTO `hirerecord` (`recordID`, `userID`, `devID`, `orderdate`) VALUES
-(11, 50, 10, '2021-09-05 08:24:59');
+(12, 49, 9, '2021-09-09 16:22:00');
 
 -- --------------------------------------------------------
 
@@ -98,6 +98,13 @@ CREATE TABLE `hires` (
   `devID` int(11) NOT NULL,
   `orderdate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hires`
+--
+
+INSERT INTO `hires` (`hireID`, `userID`, `devID`, `orderdate`) VALUES
+(12, 49, 9, '2021-09-09 16:22:00');
 
 -- --------------------------------------------------------
 
@@ -121,9 +128,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `contact`, `email`, `password`, `type`, `createdate`) VALUES
 (7, 'Main Admin', '+8801791029323', 'admin@gmail.com', '$2y$12$JeOpo3aJNBzPCv2M33NizODaKIjoglPkNmfIIL96a1WMTOZDrHlBW', 'A', '2020-04-21 10:54:26'),
-(49, 'Rahman Ali', '+11121721029', 'vv@gmail.com', '$2y$12$dJz41DgPBxBF9E9HYBLr0O0TxxeClpgsmkyy.OYmaXUPGnmzWZpP.', 'U', '2021-09-04 23:26:43'),
-(50, 'Ariful Hasan Khan', '01516773217', 'arifkhant4@gmail.com', '$2y$12$JXSoGfisoQWpcFAJVaEy3OdoAmM0GbmhmaYakWd7fcEAtVnfZW.Ja', 'U', '2021-09-05 08:23:19'),
-(51, 'tttttttttt', '+11121721029', 'tt@gmail.com', '$2y$12$4L6hQoDEeFb3rDJvf0Qkq.kLwu0eVIMA8ZpJmWijHxO0kLVc/t/Gu', 'U', '2021-09-08 18:26:32');
+(49, 'Rahman Ali', '+11121721029', 'vv@gmail.com', '$2y$12$dJz41DgPBxBF9E9HYBLr0O0TxxeClpgsmkyy.OYmaXUPGnmzWZpP.', 'U', '2021-09-04 23:26:43');
 
 --
 -- Indexes for dumped tables
@@ -168,25 +173,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `devapplications`
 --
 ALTER TABLE `devapplications`
-  MODIFY `appID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `appID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `devs`
 --
 ALTER TABLE `devs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `hirerecord`
 --
 ALTER TABLE `hirerecord`
-  MODIFY `recordID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `recordID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `hires`
 --
 ALTER TABLE `hires`
-  MODIFY `hireID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `hireID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
